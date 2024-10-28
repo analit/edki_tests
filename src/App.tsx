30f001c4779307018a5c29e2d6e04508e9cdb456
sergei <s.hariaha@d.mobilunity.com>
2024-10-28 17:04:04 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
 import './App.css';
+
+import data from "./data.json"
+import Question, { Question as QuestonType } from './Question';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        {data.questions.map((question: QuestonType, i: number) => <Question key={i} question={question} />)}
+      </Container>
     </div>
   );
 }
---
+++ src/Question.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Button, Card, Form, ListGroup } from 'react-bootstrap'
+import { CheckLg, X } from 'react-bootstrap-icons'
+
+export type Answer = {
+    number: string,
+    text: string,
+}
+
+export type Question = {
+    number: number,
+    text: string,
+    right: string,
+    answers: Answer[],
+}
+
+type Props = {
+    question: Question
+}
+
+function Question({ question }: Props) {
+    const [result, setResult] = useState<string | null>(null);
+    const [textResult, setTextResult] = useState<string | null>(null)
+
+    function onClickCheck() {
+        setTextResult(result === question.right ? "ok" : "error")
+    }
+
+    function Result({ result }: { result: string }) {
+        return (
+            result === 'ok'
+                ?
+                <CheckLg color='green' size={50} />
+                :
+                <X color='red' size={50} />
+        )
+    }
+
+    return (
+        <Card className='mt-5'>
+            <Card.Body>
+                <Card.Title>
+                    {question.number}. {question.text}
+                </Card.Title>
+                <Card.Text>
+                    <ListGroup>
+                        {question.answers.map((answer: Answer, i: number) =>
+                            <ListGroup.Item>
+                                <Form.Check
+                                    type={'radio'}
+                                    label={answer.number + ") " + answer.text}
+                                    name={'group-' + question.number}
+                                    onClick={() => setResult(answer.number)}
+                                    id={question.number + "_" + i}
+                                />
+                            </ListGroup.Item>
+                        )}
+                    </ListGroup>
+                </Card.Text>
+                <Button onClick={onClickCheck}>Check</Button>
+                {textResult ? <Result result={textResult} /> : null}
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Question
